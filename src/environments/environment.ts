c8d1fd13bf21bf58df01998fdd713316f399c2c4
Mahesh Langhe <150235242+lmahesh1@users.noreply.github.com>
2026-02-03 20:59:18 +0530

    minor changes for apiBaseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
  // apiBaseUrl: 'http://localhost:8080/rivo9'
-   apiBaseUrl:'https://rivo9fetchbrandbackendv.vercel.app/'
+   apiBaseUrl:'https://rivo9fetchbrandbackendv.vercel.app'
 };

29f4aa4e635e597d009f6c9c040f76ef7f57261a
Mahesh Langhe <150235242+lmahesh1@users.noreply.github.com>
2026-02-03 18:16:21 +0530

    Update apiBaseUrl to new gateway endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/rivo9'
+ // apiBaseUrl: 'http://localhost:8080/rivo9'
+   apiBaseUrl:'http://localhost:9090/gateway'
 };

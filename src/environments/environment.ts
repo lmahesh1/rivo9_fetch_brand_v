d96140db20adf2bd5dee4ef1634218db7b0b6d65
Mahesh Langhe <150235242+lmahesh1@users.noreply.github.com>
2026-02-04 08:43:41 +0530

    Update API base URL for environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
  // apiBaseUrl: 'http://localhost:8080/rivo9'
-   apiBaseUrl:'https://rivo9fetchbrandbackendv.vercel.app/gateway'
+   apiBaseUrl:'https://rivo9-fetch-brand-backend-new.vercel.app/gateway'
 };

f28eda93cbb1f399c98d427f353230aee192d7d9
Mahesh Langhe <150235242+lmahesh1@users.noreply.github.com>
2026-02-04 08:57:53 +0530

    Switch API base URL to localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
- // apiBaseUrl: 'http://localhost:8080/rivo9'
-   apiBaseUrl:'https://rivo9-fetch-brand-backend-new.vercel.app/gateway'
+ apiBaseUrl: 'http://localhost:8080/rivo9'
+  // apiBaseUrl:'https://rivo9-fetch-brand-backend-new.vercel.app/gateway'
 };

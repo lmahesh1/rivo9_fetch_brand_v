b3323ff52a02f0200c7d2119825cb11106a32504
Mahesh Langhe <150235242+lmahesh1@users.noreply.github.com>
2026-02-04 08:58:50 +0530

    Update apiBaseUrl to new gateway endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
- apiBaseUrl: 'http://localhost:8080/rivo9'
+   apiBaseUrl: 'http://localhost:9090/gateway'
   // apiBaseUrl:'https://rivo9-fetch-brand-backend-new.vercel.app/gateway'
 };
